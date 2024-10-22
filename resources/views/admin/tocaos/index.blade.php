1706fe158c965d6b5e45c51f50a658027b77f803
Hoàng <hoang7bvo@gmail.com>
2024-10-22 22:22:23 +0700

    Update

--- resources/views/admin/tocaos/index.blade.php
+++ resources/views/admin/tocaos/index.blade.php
@@ -61,7 +61,7 @@
                                             <button type="submit" class="btn btn-primary">Sửa Trạng Thái</button>
                                         </form>
                                     </td>
-                                    <td>
+                                    {{-- <td>
 
                                         <form action="{{ route('admin.tocaos.destroy', $complaint->id) }}" method="POST"
                                             onsubmit="return confirm('Are you sure you want to delete this complaint?');">
@@ -70,7 +70,7 @@
                                             <button type="submit" class="btn btn-danger">Delete</button>
                                         </form>
 
-                                    </td>
+                                    </td> --}}
                                 </tr>
                             @endforeach
 
